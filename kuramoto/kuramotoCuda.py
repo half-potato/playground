ea948e1334ddf1cf03e1edf8f6f8ecdfc20a7bc6
Alex Mai <alexandertmai@gmail.com>
2018-02-20 20:13:00 -0800

    Cuda support

--- kuramoto/kuramotoCuda.py
+++ kuramoto/kuramotoCuda.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cupy as cp
 import cv2, time
 import matplotlib.pyplot as plt
 import scipy.stats as st
@@ -11,8 +12,8 @@ def gkern(kernlen=21, nsig=3):
 
     interval = (2*nsig+1.)/(kernlen)
     x = np.linspace(-nsig-interval/2., nsig+interval/2., kernlen+1)
-    kern1d = np.diff(st.norm.cdf(x))
-    kernel_raw = np.sqrt(np.outer(kern1d, kern1d))
+    kern1d = cp.array(np.diff(st.norm.cdf(x)))
+    kernel_raw = cp.sqrt(cp.outer(kern1d, kern1d))
     kernel = kernel_raw/kernel_raw.sum()
     return kernel
 
@@ -22,8 +23,8 @@ class Kuramoto:
         mean: float
           The mean frequency of oscillators in hertz
         """
-        self.internal_freq = np.random.normal(mean, std, (1, size))
-        self.phase = 2*np.pi*np.random.rand(1, size)
+        self.internal_freq = cp.random.normal(mean, std, (1, size))
+        self.phase = 2*cp.pi*cp.random.rand(1, size)
         self.size = size
         self.coupling = coupling
         cv2.namedWindow("Display", cv2.WINDOW_NORMAL)
@@ -32,25 +33,25 @@ class Kuramoto:
         self.phis = []
 
     def update(self, dt):
-        phase_rep = np.repeat(self.phase, self.size, axis=0)
-        phase_diff = np.sum(self.coupling*np.sin(phase_rep.T - phase_rep), axis=0)
+        phase_rep = cp.repeat(self.phase, self.size, axis=0)
+        phase_diff = cp.sum(self.coupling*cp.sin(phase_rep.T - phase_rep), axis=0)
         dtheta = (self.internal_freq + phase_diff/self.size)
-        self.phase = np.mod(self.phase + dtheta * dt, 2*np.pi)
-        self.hist = np.vstack((self.hist, self.phase))
+        self.phase = cp.mod(self.phase + dtheta * dt, 2*cp.pi)
+        self.hist = cp.vstack((self.hist, self.phase))
         r, phi = self.order()
         self.rs.append(r)
         self.phis.append(phi)
 
     def order(self):
-        o = np.mean(np.exp(self.phase * np.complex(0,1)))
-        return abs(o), np.angle(o)
+        o = cp.mean(cp.exp(self.phase * cp.complex(0,1)))
+        return abs(o), cp.angle(o)
 
     def view(self, shape):
-        cv2.imshow("Display", norm(np.reshape(self.phase, shape)))
+        cv2.imshow("Display", norm(cp.reshape(self.phase, shape)))
 
     def view_hist(self):
         plt.subplot(3,1,1)
-        plt.plot(range(self.hist.shape[0]), np.sum(self.hist, axis=1))
+        plt.plot(range(self.hist.shape[0]), cp.sum(self.hist, axis=1))
         plt.subplot(3,1,2)
         plt.plot(range(len(self.rs)), self.rs)
         plt.subplot(3,1,3)
@@ -59,7 +60,7 @@ class Kuramoto:
         cv2.imshow("Display", norm(self.hist))
 
     def view_fft(self):
-        fft = np.fft.fft(self.phase).real
+        fft = cp.fft.fft(self.phase).real
         cv2.imshow("Display", norm(fft))
 
 """
@@ -75,32 +76,32 @@ def shift(a, num, axis):
 """
 
 def shift(a, x, y):
-    b = np.roll(a, x, axis=1)
+    b = cp.roll(a, x, axis=1)
     if x >= 0:
-        b[:, 0:x] = np.zeros((a.shape[0], abs(x)))
+        b[:, 0:x] = cp.zeros((a.shape[0], abs(x)))
     else:
-        b[:, x:] = np.zeros((a.shape[0], abs(x)))
+        b[:, x:] = cp.zeros((a.shape[0], abs(x)))
 
-    b = np.roll(b, y, axis=0)
+    b = cp.roll(b, y, axis=0)
     if y >= 0:
-        b[0:y, :] = np.zeros((abs(y), a.shape[1]))
+        b[0:y, :] = cp.zeros((abs(y), a.shape[1]))
     else:
-        b[y:, :] = np.zeros((abs(y), a.shape[1]))
+        b[y:, :] = cp.zeros((abs(y), a.shape[1]))
     return b
 
 def ring_coupling(shape, weights):
-    e = np.eye(*shape)
+    e = cp.eye(*shape)
     c = e.copy()
     for i in range(len(weights)-1):
-        #c += weights[i]*np.roll(e, i+1, axis=0)
-        #c += weights[i]*np.roll(e, i+1, axis=1)
+        #c += weights[i]*cp.roll(e, i+1, axis=0)
+        #c += weights[i]*cp.roll(e, i+1, axis=1)
         c += weights[i]*shift(e, i+1, 0)
         c += weights[i]*shift(e, 0, i+1)
     return c
 
 def local_coupling(shape, kernel):
     axis_pad = ((0,shape[0]-kernel.shape[0]),(0, shape[1]-kernel.shape[1]))
-    k_pad = np.pad(kernel, axis_pad, "constant", constant_values=(0))
+    k_pad = cp.pad(kernel, axis_pad, "constant", constant_values=(0))
     coupling = []
     for i in range(shape[0]):
         for j in range(shape[1]):
@@ -108,7 +109,7 @@ def local_coupling(shape, kernel):
             print(i, j)
             k_shifted = shift(k_pad, i-kernel.shape[1]//2, j-kernel.shape[0]//2)
             coupling.append(k_shifted.flatten())
-    return np.array(coupling)
+    return cp.array(coupling)
 
 if __name__ == "__main__":
     size = 40
@@ -116,6 +117,8 @@ if __name__ == "__main__":
     #coupling = np.random.normal(1, .5, (size**2, size**2))
     #coupling = ring_coupling((size**2, size**2), [1, 1, 1, 0.75, .5, 0.25])
     coupling = local_coupling((size, size), gkern(5, 2))
+    #coupling = cp.array(coupling.tolist())
+    print(coupling)
     #coupling = ring_coupling((size**2, size**2), [1, 1])
     k = Kuramoto(size**2, 1, 0.50, 150*coupling)
     dt = .050
